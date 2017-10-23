bb0fe6b8911360061741df42b7ab6f773a514a5c
User <User@192.168.10.32>
2017-10-23 18:26:17 +0300

    Session Making SessionListerner MenuControl Develop1

--- src/java/pns/kiam/Utils/NumberUtils.java
+++ src/java/pns/kiam/Utils/NumberUtils.java
@@ -15,64 +15,64 @@ import java.util.List;
 public class NumberUtils {
 
     public static List<Double> arrayToList(double[] arr) {
-	List<Double> res = new ArrayList<>();
-	for (int k = 0; k < arr.length; k++) {
-	    res.add(arr[k]);
-	}
-	return res;
+        List<Double> res = new ArrayList<>();
+        for (int k = 0; k < arr.length; k++) {
+            res.add(arr[k]);
+        }
+        return res;
     }
 
     public static List<Double> excludefromList(List<Double> baseList, List<Double> exclusionList) {
-	List<Double> res = new ArrayList<>();
-	for (int k = 0; k < baseList.size(); k++) {
-	    if (!exclusionList.contains(baseList.get(k))) {
-		res.add(baseList.get(k));
-	    }
-	}
-	return res;
+        List<Double> res = new ArrayList<>();
+        for (int k = 0; k < baseList.size(); k++) {
+            if (!exclusionList.contains(baseList.get(k))) {
+                res.add(baseList.get(k));
+            }
+        }
+        return res;
     }
 
     public static double[] excludefromList(double[] base, double[] exclusion) {
-	List<Double> baseList = arrayToList(base);
-	List<Double> exclusionList = arrayToList(exclusion);
-	List<Double> resList = excludefromList(baseList, exclusionList);
-	double[] res = new double[resList.size()];
-	for (int k = 0; k < resList.size(); k++) {
-	    res[k] = resList.get(k);
-	}
-	return res;
+        List<Double> baseList = arrayToList(base);
+        List<Double> exclusionList = arrayToList(exclusion);
+        List<Double> resList = excludefromList(baseList, exclusionList);
+        double[] res = new double[resList.size()];
+        for (int k = 0; k < resList.size(); k++) {
+            res[k] = resList.get(k);
+        }
+        return res;
     }
 
     public static double[] excludefromList(double[] base, List<Double> exclusionList) {
-	List<Double> baseList = arrayToList(base);
-	List<Double> resList = excludefromList(baseList, exclusionList);
-	double[] res = new double[resList.size()];
-	for (int k = 0; k < resList.size(); k++) {
-	    res[k] = resList.get(k);
-	}
-	return res;
+        List<Double> baseList = arrayToList(base);
+        List<Double> resList = excludefromList(baseList, exclusionList);
+        double[] res = new double[resList.size()];
+        for (int k = 0; k < resList.size(); k++) {
+            res[k] = resList.get(k);
+        }
+        return res;
     }
 
     public static void outputForPrintDoubleList(List<Double> baseList) {
-	System.out.println("Double List " + baseList.getClass().getSimpleName() + ". size: " + baseList.size());
-	for (int k = 0; k < baseList.size(); k++) {
-	    System.out.println(k + ": " + baseList.get(k));
-	}
+        System.out.println("Double List " + baseList.getClass().getSimpleName() + ". size: " + baseList.size());
+        for (int k = 0; k < baseList.size(); k++) {
+            System.out.println(k + ": " + baseList.get(k));
+        }
     }
 
     public static double[] copyArrayToArray(double[] source) {
-	double[] res = new double[source.length];
-	for (int k = 0; k < source.length; k++) {
-	    res[k] = source[k];
-	}
-	return res;
+        double[] res = new double[source.length];
+        for (int k = 0; k < source.length; k++) {
+            res[k] = source[k];
+        }
+        return res;
     }
 
     public static void outputForPrintDoubleArray(double[] base) {
-	System.out.println("Double Array " + base.getClass().getSimpleName() + ". length " + base.length);
-	for (int k = 0; k < base.length; k++) {
-	    System.out.println(k + ": " + base[k]);
-	}
+        System.out.println("Double Array " + base.getClass().getSimpleName() + ". length " + base.length);
+        for (int k = 0; k < base.length; k++) {
+            System.out.println(k + ": " + base[k]);
+        }
     }
 
     /**
@@ -85,31 +85,31 @@ public class NumberUtils {
      * @return
      */
     public static double[] generateDoubleArray(double minVal, double maxVal, double step) {
-	int dim = 1;
-	if (step > 0) {
-	    dim = (int) (Math.abs(minVal - maxVal) / step) + 1;
-	}
-	double[] res = new double[dim];
-	res[0] = minVal;
-	for (int k = 1; k < dim; k++) {
-	    res[k] = res[k - 1] + step;
-	}
-	return res;
+        int dim = 1;
+        if (step > 0) {
+            dim = (int) (Math.abs(minVal - maxVal) / step) + 1;
+        }
+        double[] res = new double[dim];
+        res[0] = minVal;
+        for (int k = 1; k < dim; k++) {
+            res[k] = res[k - 1] + step;
+        }
+        return res;
     }
 
     public static String genRandomSuperBigInt() {
-	String res = ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
-	res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        String res = ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
+        res += ((int) (Integer.MAX_VALUE * Math.random() / 3)) + "";
 
-	return res;
+        return res;
     }
 
 }
