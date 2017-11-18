6b0716ca1f80e35833b1859c62364fba5678e259
PSEVO tochka <PSEVO tochka@polutoshiba1>
2017-11-18 12:31:54 +0300

    11-11-17  Refresh Users

--- src/java/pns/kiam/entities/satellites/FileMeasured.java
+++ src/java/pns/kiam/entities/satellites/FileMeasured.java
@@ -11,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -28,81 +29,86 @@ public class FileMeasured implements Serializable {
     private int year = 0;
     private int month = 0;
     private int date = 0;
-    private String content;
+
+    @Size(max = 20480)
+    private String content = "";
+
+    public FileMeasured() {
+    }
 
     public FileMeasured(int y, int m, int d, String c) {
-        year = y;
-        month = m;
-        date = d;
-        content = c;
+	year = y;
+	month = m;
+	date = d;
+	content = c;
     }
 
     public Long getId() {
-        return id;
+	return id;
     }
 
     public void setId(Long id) {
-        this.id = id;
+	this.id = id;
     }
 
     public int getYear() {
-        return year;
+	return year;
     }
 
     public void setYear(int year) {
-        this.year = year;
+	this.year = year;
     }
 
     public int getMonth() {
-        return month;
+	return month;
     }
 
     public void setMonth(int month) {
-        this.month = month;
+	this.month = month;
     }
 
     public int getDate() {
-        return date;
+	return date;
     }
 
     public void setDate(int date) {
-        this.date = date;
+	this.date = date;
     }
 
     public String getContent() {
-        return content;
+	return content;
     }
 
     public void setContent(String content) {
-        this.content = content;
+	this.content = content;
     }
 
     @Override
     public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+	int hash = 0;
+	hash += (id != null ? id.hashCode() : 0);
+	return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof FileMeasured)) {
-            return false;
-        }
-        FileMeasured other = (FileMeasured) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        if ((this.content == null && other.content != null) || (this.content != null && !this.content.trim().equals(other.content.trim()))) {
-            return false;
-        }
-        return true;
+	// TODO: Warning - this method won't work in the case the id fields are not set
+	if (!(object instanceof FileMeasured)) {
+	    return false;
+	}
+	FileMeasured other = (FileMeasured) object;
+	if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+	    return false;
+	}
+	if ((this.content == null && other.content != null) || (this.content != null && !this.content.trim().equals(other.content.trim()))) {
+	    return false;
+	}
+	return true;
     }
 
     @Override
     public String toString() {
-        return "FileMeasured[ id=" + id + " ]";
+	return "FileMeasured[ id=" + id + " ]";
     }
 
 }
